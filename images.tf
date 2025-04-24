5544b8e7f8ede29b727ce40dd123637f430b6d07
박금혁 <ghpark@enki.co.kr>
2025-04-24 19:58:41 +0900

    Update image source URLs to local server for preinstalled Debian 11 and Kali images

--- images.tf
+++ images.tf
@@ -45,7 +45,7 @@ resource "openstack_images_image_v2" "debian_11_man" {
 resource "openstack_images_image_v2" "preinstalled_debian_11_man" {
   count            = var.preinstalled_man ? 1 : 0
   name             = "debian-11-man-preinstalled"
-  image_source_url = "https://object-store.cloud.muni.cz/swift/v1/kypo-images/debian-11-man.qcow2"
+  image_source_url = "http://10.4.1.63/debian-11-man.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
 
@@ -57,7 +57,7 @@ resource "openstack_images_image_v2" "preinstalled_debian_11_man" {
 resource "openstack_images_image_v2" "kali" {
   count            = var.kali ? 1 : 0
   name             = "kali"
-  image_source_url = "https://object-store.cloud.muni.cz/swift/v1/kypo-images/kali.qcow2"
+  image_source_url = "http://10.4.1.63/kali.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
 
